731e26b2996b290b22fc81314a74f4d2dbd5bc5a
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-02-25 17:41:31 -0500

    module not found bug1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
+import FaceRecognition from './components/facerecognition/FaceRecognition.js';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 
