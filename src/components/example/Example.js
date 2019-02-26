fac5d5557f5dcdb2bb12e7a5b5b02c81669f2717
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-02-26 02:33:01 -0500

    styles example images

--- src/components/example/Example.js
+++ src/components/example/Example.js
@@ -5,22 +5,22 @@ class Example extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <p className='f3'>
+        <p className='f2 mh4'>
           {'This Magic Brain will detect faces in your pictures. Enter a URL of an image that has a face in it. Maybe something like these ...'}
         </p> <
         div className = "flex flex-wrap items-center justify-center" >
-        <div className="center br3 shadow-5 pa2">
+        <div className="flex-column pa4 ma4 center br3 pa2">
             <img src="https://picsum.photos/500?image=1062" alt="wrapped dog"/>
             <p className="center">https://picsum.photos/500?image=1062</p>
           </div> <
-        div className = "center br3 shadow-5 pa2" >
+        div className = "flex-column pa4 ma4 center br3 pa2" >
         <img src="https://picsum.photos/500/?image=836" alt="woman playing guitar" /> <
-        p className = "center" > https: //picsum.photos/500/?image=836</p>
+        p className = "center" > https://picsum.photos/500/?image=836</p>
         <
         /div> <
-        div className = "center br3 shadow-5 pa2" >
+        div className = "flex-column pa4 ma4 center br3 pa2" >
         <img src="https://picsum.photos/500/?image=804" alt="man napping on steering wheel" /> <
-        p className = "center" > https: //picsum.photos/500/?image=804</p>
+        p className = "center" > https://picsum.photos/500/?image=804</p>
         <
         /div> < /
         div >
